10bdd28c0eb20d5f17a68d3d7a276fd015c10c65
KATO <osama.s.abdelmoniem@gmail.com>
2023-04-21 02:11:42 +0200

    add host vans page

---
+++ src/pages/Host/HostVans.jsx
@@ -0,0 +1,46 @@
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import classes from '../../index.module.css';
+const HostVans = () => {
+    const [vans, setVans] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('/api/host/vans');
+            const data = await response.json();
+            setVans(data.vans);
+        };
+        fetchData();
+    }, []);
+
+    const hostVansEls = vans.map((van) => (
+        <Link
+            to={`/host/vans/${van.id}`}
+            key={van.id}
+            className={classes['host-van-link-wrapper']}
+        >
+            <div className={classes['host-van-single']} key={van.id}>
+                <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+                <div className={classes['host-van-info']}>
+                    <h3>{van.name}</h3>
+                    <p>${van.price}/day</p>
+                </div>
+            </div>
+        </Link>
+    ));
+
+    return (
+        <section>
+            <h1 className='host-vans-title'>Your listed vans</h1>
+            <div className='host-vans-list'>
+                {vans.length > 0 ? (
+                    <section>{hostVansEls}</section>
+                ) : (
+                    <h2>Loading...</h2>
+                )}
+            </div>
+        </section>
+    );
+};
+
+export default HostVans;
