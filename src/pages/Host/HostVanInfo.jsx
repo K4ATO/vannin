6525c6ef415e6fef95acad6bb88f1e99756e205e
KATO <osama.s.abdelmoniem@gmail.com>
2023-04-21 03:17:45 +0200

    add host van info page

---
+++ src/pages/Host/HostVanInfo.jsx
@@ -0,0 +1,22 @@
+import classes from '../../index.module.css';
+import { useOutletContext } from 'react-router-dom';
+const HostVanInfo = () => {
+    const { currentVan } = useOutletContext();
+    return (
+        <section className={classes['host-van-detail-info']}>
+            <h4>
+                Name: <span>{currentVan.name}</span>
+            </h4>
+            <h4>
+                Category: <span>{currentVan.type}</span>
+            </h4>
+            <h4>
+                Description: <span>{currentVan.description}</span>
+            </h4>
+            <h4>
+                Visibility: <span>Public</span>
+            </h4>
+        </section>
+    );
+};
+export default HostVanInfo;
