5843e741e23a6e811f31a01c8de731bc78a07aa0
KATO <osama.s.abdelmoniem@gmail.com>
2023-04-21 03:19:21 +0200

    add host routes to the app

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,9 @@ import Income from './pages/Host/Income';
 import Reviews from './pages/Host/Reviews';
 import HostVansDetail from './pages/Host/HostVansDetail';
 import HostVans from './pages/Host/HostVans';
+import HostVanInfo from './pages/Host/HostVanInfo';
+import HostVanPricing from './pages/Host/HostVanPricing';
+import HostVanPhotos from './pages/Host/HostVanPhotos';
 function App() {
     return (
         <>
@@ -22,7 +25,14 @@ function App() {
                         <Route path='income' element={<Income />} />
                         <Route path='reviews' element={<Reviews />} />
                         <Route path='vans' element={<HostVans />} />
-                        <Route path='vans/:id' element={<HostVansDetail />} />
+                        <Route path='vans/:id' element={<HostVansDetail />}>
+                            <Route index element={<HostVanInfo />} />
+                            <Route
+                                path='pricing'
+                                element={<HostVanPricing />}
+                            />
+                            <Route path='photos' element={<HostVanPhotos />} />
+                        </Route>
                     </Route>
                     <Route path='/vans' element={<Vans />} />
                     <Route path='/vans/:id/' element={<VanDetail />} />
