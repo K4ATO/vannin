1808e03b4bf6580e11ffff546bb5fef7bf3e5493
KATO <osama.s.abdelmoniem@gmail.com>
2023-04-21 16:49:36 +0200

    add search parameters

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
 import classes from '../../index.module.css';
 import Van from '../../components/Van';
+import { useSearchParams, Link } from 'react-router-dom';
 const Vans = () => {
     const [vans, setVans] = useState();
-
+    const [searchParams, setSearchParams] = useSearchParams();
+    const typeFilter = searchParams.get('type');
     useEffect(() => {
         const fetchData = async () => {
             const response = await fetch('/api/vans');
@@ -13,20 +15,77 @@ const Vans = () => {
         fetchData();
     }, []);
 
+    const handleFilterChange = (key, value) => {
+        setSearchParams((prevParams) => {
+            if (value === null) {
+                prevParams.delete(key);
+            } else {
+                prevParams.set(key, value);
+            }
+            return prevParams;
+        });
+    };
+
     return (
         <div className={classes['van-list-container']}>
             <h1>Explore our van options</h1>
+            <div className={classes['van-list-filter-buttons']}>
+                <button
+                    onClick={() => handleFilterChange('type', 'simple')}
+                    className={`${classes['van-type']} ${classes['simple']} ${
+                        typeFilter === 'simple' ? classes.selected : ''
+                    }`}
+                >
+                    Simple
+                </button>
+                <button
+                    onClick={() => handleFilterChange('type', 'luxury')}
+                    className={`${classes['van-type']} ${classes['luxury']} ${
+                        typeFilter === 'luxury' ? classes.selected : ''
+                    }`}
+                >
+                    Luxury
+                </button>
+                <button
+                    onClick={() => handleFilterChange('type', 'rugged')}
+                    className={`${classes['van-type']} ${classes['rugged']} ${
+                        typeFilter === 'rugged' ? classes.selected : ''
+                    }`}
+                >
+                    Rugged
+                </button>
+                {typeFilter ? (
+                    <button
+                        onClick={() => handleFilterChange('type', null)}
+                        className={`${classes['van-type']} ${classes['clear-filters']}`}
+                    >
+                        Clear filter
+                    </button>
+                ) : null}
+            </div>
             <div className={classes['van-list']}>
-                {vans &&
-                    vans.map((van) => (
-                        <Van
-                            id={van.id}
-                            imageUrl={van.imageUrl}
-                            name={van.name}
-                            price={van.price}
-                            type={van.type}
-                        />
-                    ))}
+                {vans && typeFilter
+                    ? vans
+                          .filter((van) => van.type === typeFilter)
+                          .map((van) => (
+                              <Van
+                                  id={van.id}
+                                  imageUrl={van.imageUrl}
+                                  name={van.name}
+                                  price={van.price}
+                                  type={van.type}
+                              />
+                          ))
+                    : vans &&
+                      vans.map((van) => (
+                          <Van
+                              id={van.id}
+                              imageUrl={van.imageUrl}
+                              name={van.name}
+                              price={van.price}
+                              type={van.type}
+                          />
+                      ))}
             </div>
         </div>
     );
