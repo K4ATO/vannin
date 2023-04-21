438db2a5bc44f937398299df4c67b60c13788a15
KATO <osama.s.abdelmoniem@gmail.com>
2023-04-22 00:11:26 +0200

    add host vans fetch function

--- src/utility/vansApi.js
+++ src/utility/vansApi.js
@@ -1,13 +1,28 @@
 /* eslint-disable no-throw-literal */
-export const getVans = async () => {
-    const response = await fetch('/api/vans');
-    if (!response.ok) {
+export async function getVans(id) {
+    const url = id ? `/api/vans/${id}` : '/api/vans';
+    const res = await fetch(url);
+    if (!res.ok) {
         throw {
             message: 'Failed to fetch vans',
-            statusText: response.statusText,
-            status: response.status,
+            statusText: res.statusText,
+            status: res.status,
         };
     }
-    const data = await response.json();
+    const data = await res.json();
     return data.vans;
-};
+}
+
+export async function getHostVans(id) {
+    const url = id ? `/api/host/vans/${id}` : '/api/host/vans';
+    const res = await fetch(url);
+    if (!res.ok) {
+        throw {
+            message: 'Failed to fetch vans',
+            statusText: res.statusText,
+            status: res.status,
+        };
+    }
+    const data = await res.json();
+    return data.vans;
+}
