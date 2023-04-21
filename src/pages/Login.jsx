e345551b88ec727836533c3c38c88439baf00754
KATO <osama.s.abdelmoniem@gmail.com>
2023-04-22 01:51:58 +0200

    add fake login func

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,46 +1,52 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import classes from '../index.module.css';
+import {
+    useLoaderData,
+    Form,
+    redirect,
+    useActionData,
+    useNavigation,
+} from 'react-router-dom';
+import { loginUser } from '../utility/loginApi';
 
+export const loginAction = async ({ request }) => {
+    const formData = await request.formData();
+    const email = formData.get('email');
+    const password = formData.get('password');
+    const pathname =
+        new URL(request.url).searchParams.get('redirectTo') || '/host';
+    try {
+        const data = await loginUser({ email, password });
+        localStorage.setItem('loggedin', true);
+        return redirect(pathname);
+    } catch (error) {
+        return error.message;
+    }
+};
+
+export const loginLoader = ({ request }) => {
+    return new URL(request.url).searchParams.get('message');
+};
 const Login = () => {
-    const [loginFormData, setLoginFormData] = useState({
-        email: '',
-        password: '',
-    });
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log(loginFormData);
-    }
-
-    function handleChange(e) {
-        const { name, value } = e.target;
-        setLoginFormData((prev) => ({
-            ...prev,
-            [name]: value,
-        }));
-    }
+    const message = useLoaderData();
+    const errorMessage = useActionData();
+    const navigation = useNavigation();
 
     return (
         <div className={classes['login-container']}>
             <h1>Sign in to your account</h1>
-            <form onSubmit={handleSubmit} className={classes['login-form']}>
-                <input
-                    name='email'
-                    onChange={handleChange}
-                    type='email'
-                    placeholder='Email address'
-                    value={loginFormData.email}
-                />
-                <input
-                    name='password'
-                    onChange={handleChange}
-                    type='password'
-                    placeholder='Password'
-                    value={loginFormData.password}
-                />
-                <button>Log in</button>
-            </form>
+            {message && <h3 className={classes['login-error']}>{message}</h3>}
+            {errorMessage && (
+                <h3 className={classes['login-error']}>{errorMessage}</h3>
+            )}
+            <Form method='post' className={classes['login-form']}>
+                <input name='email' type='email' placeholder='Email address' />
+                <input name='password' type='password' placeholder='Password' />
+                <button disabled={navigation.state === 'submitting'}>
+                    {navigation.state === 'submitting'
+                        ? 'Logging in...'
+                        : 'Log in'}
+                </button>
+            </Form>
         </div>
     );
 };
