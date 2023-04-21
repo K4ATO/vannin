0053a32c78d9a0eb604ddc25001a91daaf2e6800
KATO <osama.s.abdelmoniem@gmail.com>
2023-04-21 18:49:49 +0200

    add login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import classes from '../index.module.css';
+
+const Login = () => {
+    const [loginFormData, setLoginFormData] = useState({
+        email: '',
+        password: '',
+    });
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(loginFormData);
+    }
+
+    function handleChange(e) {
+        const { name, value } = e.target;
+        setLoginFormData((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+    }
+
+    return (
+        <div className={classes['login-container']}>
+            <h1>Sign in to your account</h1>
+            <form onSubmit={handleSubmit} className={classes['login-form']}>
+                <input
+                    name='email'
+                    onChange={handleChange}
+                    type='email'
+                    placeholder='Email address'
+                    value={loginFormData.email}
+                />
+                <input
+                    name='password'
+                    onChange={handleChange}
+                    type='password'
+                    placeholder='Password'
+                    value={loginFormData.password}
+                />
+                <button>Log in</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
